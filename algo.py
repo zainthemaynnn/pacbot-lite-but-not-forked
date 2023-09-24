00412a07d1d776f3742994e18dc9f0960a2c19db
Zain Azam <zainyusufazam@gmail.com>
2023-09-24 02:57:10 -0400

    graph

--- algo.py
+++ algo.py
@@ -1,16 +1,113 @@
 # You can modify this file to implement your own algorithm
 
 from constants import *
+from grid import grid
+import numpy as np
+from enum import Enum
+
+Direction = Enum("Direction", ["UP", "DOWN", "LEFT", "RIGHT"])
+
+directions = [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT]
+
+
+def traversible(space):
+    """whether a space is traversible"""
+    return space == o or space == O or space == e or space == c
+
+
+def adjacent(grid, x, y):
+    """returns traversible adjacent spaces"""
+    adj = []
+    for d in directions:
+        t = dvec(d)
+        if traversible(grid[x + t[0]][y + t[1]]):
+            adj.append(d)
+    return adj
+
+
+def neg(v):
+    """negates tuple vector"""
+    return (-v[0], -v[1])
+
+
+def dvec(direction):
+    """converts direction to a tuple vector"""
+    if direction == Direction.UP:
+        return (0, -1)
+    elif direction == Direction.DOWN:
+        return (0, 1)
+    elif direction == Direction.LEFT:
+        return (-1, 0)
+    else:
+        return (1, 0)
+
+
+class Graph:
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
+
+
+class Edge:
+    def __init__(self, src, dst, weight):
+        self.src = src
+        self.dst = dst
+        self.weight = weight
+
+
+def construct_graph(grid):
+    visited = {}
+    nodes = []
+    edges = []
+
+    nodes.append((1, 1))
+    visited[(1, 1)] = True
+    queue = [((1, 1), adjacent(grid, 1, 1))]
+
+    while len(queue) > 0:
+        for q in list(queue):
+            queue.pop(0)
+            o = q[0]
+            paths = q[1]
+
+            for d in paths:
+                p = o
+                t = dvec(d)
+                weight = 0
+
+                while weight == 0 or len(adjacent(grid, p[0], p[1])) == 2:
+                    if not traversible(grid[p[0] + t[0]][p[1] + t[1]]):
+                        t = dvec(
+                            next(
+                                filter(
+                                    lambda nd: t != neg(dvec(nd)),
+                                    adjacent(grid, p[0], p[1]),
+                                )
+                            )
+                        )
+                    p = (p[0] + t[0], p[1] + t[1])
+                    weight += 1
+
+                if weight > 0:
+                    if not visited.get(p):
+                        visited[p] = True
+                        nodes.append(p)
+                        queue.append((p, adjacent(grid, p[0], p[1])))
+                    # probably a faster way to do this, but eh
+                    edges.append(Edge(nodes.index(o), nodes.index(p), weight))
+
+    return Graph(nodes, edges)
+
 
 """
 You can use the following values from constants.py to check for the type of cell in the grid:
-I = 1 -> Wall 
+I = 1 -> Wall
 o = 2 -> Pellet (Small Dot)
 e = 3 -> Empty
 """
 
-def get_next_coordinate(grid, location):
 
+def get_next_coordinate(grid, location):
     """
     Calculate the next coordinate for 6ix-pac to move to.
     Check if the next coordinate is a valid move.
@@ -20,9 +117,9 @@ def get_next_coordinate(grid, location):
     - location (list): The current location of the 6ix-pac in the form (x, y).
 
     Returns:
-    - list or tuple: 
+    - list or tuple:
         - If the next coordinate is valid, return the next coordinate in the form (x, y) or [x,y].
         - If the next coordinate is invalid, return None.
     """
-    
+
     return location
--- game.py
+++ game.py
@@ -1,16 +1,15 @@
-
 # DO NOT MODIFY THIS FILE
 
 import pygame
 import sys
 import math
 from grid import grid
-from algo import get_next_coordinate
+from algo import get_next_coordinate, construct_graph
 from constants import BLACK, BLUE, RED, WHITE, YELLOW, o, e, I, O, c
 
-GRID_SIZE = 20  
-GRID_WIDTH = len(grid)  
-GRID_HEIGHT = len(grid[0]) 
+GRID_SIZE = 20
+GRID_WIDTH = len(grid)
+GRID_HEIGHT = len(grid[0])
 
 pygame.init()
 
@@ -24,11 +23,13 @@ pacman_y = 7
 
 clock = pygame.time.Clock()
 
-start_time = pygame.time.get_ticks()  
-score = 0 
+start_time = pygame.time.get_ticks()
+score = 0
 
 # Main game loop
 running = True
+
+graph = construct_graph(grid)
 
 while running:
     for event in pygame.event.get():
@@ -39,11 +40,11 @@ while running:
     screen.fill(BLACK)
 
     current_time = pygame.time.get_ticks()
-    elapsed_time = (current_time - start_time) // 1000 
+    elapsed_time = (current_time - start_time) // 1000
 
     current_x, current_y = pacman_x, pacman_y
-    path = get_next_coordinate(grid, (current_x, current_y))
-    
+    path = get_next_coordinate(graph, (current_x, current_y))
+
     if path is None or len(path) == 0 or path[0] is None:
         print("Invalid Path. Try something else")
         running = False
@@ -60,12 +61,11 @@ while running:
         orientation_angle = -90
     elif pacman_y > current_y:
         orientation_angle = 90
-    
-    
+
     if grid[pacman_x][pacman_y] == o:
         grid[pacman_x][pacman_y] = e
         score += 10
-        
+
     # Draw the grid
     pellet = False
     for row in range(GRID_WIDTH):
@@ -79,34 +79,64 @@ while running:
                 pygame.draw.rect(screen, BLACK, (cell_x, cell_y, GRID_SIZE, GRID_SIZE))
             elif grid_unit == o:
                 pellet = True
-                pygame.draw.circle(screen, WHITE, (cell_x + GRID_SIZE // 2, cell_y + GRID_SIZE // 2), 5)
+                pygame.draw.circle(
+                    screen, WHITE, (cell_x + GRID_SIZE // 2, cell_y + GRID_SIZE // 2), 5
+                )
             elif grid_unit == O:
-                pygame.draw.circle(screen, WHITE, (cell_x + GRID_SIZE // 2, cell_y + GRID_SIZE // 2), 10)
+                pygame.draw.circle(
+                    screen,
+                    WHITE,
+                    (cell_x + GRID_SIZE // 2, cell_y + GRID_SIZE // 2),
+                    10,
+                )
             elif grid_unit == c:
-                pygame.draw.circle(screen, RED, (cell_x + GRID_SIZE // 2, cell_y + GRID_SIZE // 2), 5)
+                pygame.draw.circle(
+                    screen, RED, (cell_x + GRID_SIZE // 2, cell_y + GRID_SIZE // 2), 5
+                )
 
     if not pellet:
         running = False
 
-   # Draw Pac-Man body
-    pacman_radius = GRID_SIZE // 2 - 2 
-    pacman_rect = pygame.Rect(pacman_x * GRID_SIZE, pacman_y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
-    pygame.draw.circle(screen, YELLOW, (pacman_x * GRID_SIZE + GRID_SIZE // 2, pacman_y * GRID_SIZE + GRID_SIZE // 2), pacman_radius)
+    # Draw Pac-Man body
+    pacman_radius = GRID_SIZE // 2 - 2
+    pacman_rect = pygame.Rect(
+        pacman_x * GRID_SIZE, pacman_y * GRID_SIZE, GRID_SIZE, GRID_SIZE
+    )
+    pygame.draw.circle(
+        screen,
+        YELLOW,
+        (pacman_x * GRID_SIZE + GRID_SIZE // 2, pacman_y * GRID_SIZE + GRID_SIZE // 2),
+        pacman_radius,
+    )
 
     # Draw Pac-Man mouth
-    mouth_points = [(pacman_x * GRID_SIZE + GRID_SIZE // 2, pacman_y * GRID_SIZE + GRID_SIZE // 2)]
-    mouth_angle = 40 
-    for angle in range(orientation_angle - mouth_angle, orientation_angle + mouth_angle + 1):
-        x = pacman_x * GRID_SIZE + GRID_SIZE // 2 + pacman_radius * math.cos(math.radians(angle))
-        y = pacman_y * GRID_SIZE + GRID_SIZE // 2 + pacman_radius * math.sin(math.radians(angle))
+    mouth_points = [
+        (pacman_x * GRID_SIZE + GRID_SIZE // 2, pacman_y * GRID_SIZE + GRID_SIZE // 2)
+    ]
+    mouth_angle = 40
+    for angle in range(
+        orientation_angle - mouth_angle, orientation_angle + mouth_angle + 1
+    ):
+        x = (
+            pacman_x * GRID_SIZE
+            + GRID_SIZE // 2
+            + pacman_radius * math.cos(math.radians(angle))
+        )
+        y = (
+            pacman_y * GRID_SIZE
+            + GRID_SIZE // 2
+            + pacman_radius * math.sin(math.radians(angle))
+        )
         mouth_points.append((x, y))
 
-    mouth_points.append((pacman_x * GRID_SIZE + GRID_SIZE // 2, pacman_y * GRID_SIZE + GRID_SIZE // 2))
+    mouth_points.append(
+        (pacman_x * GRID_SIZE + GRID_SIZE // 2, pacman_y * GRID_SIZE + GRID_SIZE // 2)
+    )
     pygame.draw.polygon(screen, BLACK, mouth_points)
 
     font = pygame.font.Font(None, 24)
     score_text = font.render(f"Score: {score}", True, WHITE)
-    screen.blit(score_text, (4, 16*17))
+    screen.blit(score_text, (4, 16 * 17))
 
     time_text = font.render("Time:", True, WHITE)
     screen.blit(time_text, (4, 340))
